ad65031790c62f46abd663deda4de5d3d181385c
Rhulken <48840117+Rhulken@users.noreply.github.com>
2019-04-09 00:33:47 +0200

    Add files via upload

--- Graph.py
+++ Graph.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
-import pygal
-import sqlite3
 import os, sys
+import sqlite3
+import pygal
 from datetime import datetime, timedelta
 
 def graph():
-        connect = sqlite3.connect('sonde.db')
-        c = connect.cursor() 
+        connection = sqlite3.connect('sonde.db')
+
+        c = connection.cursor() 
+
+        #declaration des list dont on a besoin
         hostnames = list()
-        n=5
+        n=1
 
         #--------------------------------------------------------------------------------------------
-        #   création d'un graph contenant les données du pc
+        #Les fonctions qui vont suivre vont permettre de créer un graphique des données suivantes :
+        #   - %Disque utilisé,
+        #   - %Ram utilisé,
+        #   - %CPU utilisé,
+        #   - SWAP total,
+        #   - Nombre utilisateur connectés
+        #Les graphiques seront crées sous forme de page HTML, elles vont se mettre à jour a chaque
+        #fois que les sondes vont être relancés
         #--------------------------------------------------------------------------------------------
 
         for row in c.execute('SELECT DISTINCT hostname FROM sonde'):
             hostnames.append(row)
-
+        data = [None] * 6
         for name in hostnames:
-            for row in c.execute("""SELECT utilisateur, ram, disque, cpu, swap time FROM sonde"""):
-                    data = c.fetchone()
-                    bar_chart = pygal.Bar()
-                    bar_chart.title = 'Donnees utilisateur'
-                    bar_chart.x_labels = map(str, range(1,n))
-                    bar_chart.add('Utilisateur', data[0])
-                    bar_chart.add('Swap', data[4])
-                    bar_chart.add('CPU',  data[3])
-                    bar_chart.add('Ram', data[1])
-                    bar_chart.add('Disque', data[2])
-                    bar_chart.render_in_browser()
+            for row in c.execute('''SELECT utilisateur, ram, disque, cpu, swap, time FROM sonde'''):
+                bar_chart = pygal.Bar()
+                bar_chart.title = 'Donnees utilisateur'
+                bar_chart.x_labels = map(str, range(1,n))
+                bar_chart.add('Utilisateur', row[0])
+                bar_chart.add('Swap', row[4])
+                bar_chart.add('CPU',  row[3])
+                bar_chart.add('Ram', row[1])
+                bar_chart.add('Disque', row[2])
+                bar_chart.render_to_file("bar_chart.svg")
+                bar_chart.render_in_browser()
